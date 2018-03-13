8398adb8897a4df1905b0930bc5152da9d87f1c3
StartMilandr <Info@StartMilandr.ru>
2018-03-13 12:19:26 +0300

    - Стартовое состояние

---
+++ Timer_EventClear/main.c
@@ -0,0 +1,90 @@
+#include "brdDef.h"
+
+#include "brdClock.h"
+#include "brdLed.h"
+#include "brdTimer.h"
+#include "brdUtils.h"
+
+#include "brdTimer_Select.h"
+
+//  ------------    BUG ACTIVATE----------
+
+#define BUG_ACTIVATE
+
+//  --------------------------------
+
+#ifdef USE_BOARD_VE_92
+  #define  LED_SEL_ALL         (BRD_LED_1 | BRD_LED_2)
+  #define  LED_COUNT           2
+  uint32_t LED_Sel[LED_COUNT] = {BRD_LED_1, BRD_LED_2};
+
+#else
+  #define  LED_SEL_ALL         (BRD_LED_1 | BRD_LED_2 | BRD_LED_3 | BRD_LED_4)
+  #define  LED_COUNT           4
+  uint32_t LED_Sel[LED_COUNT] = {BRD_LED_1, BRD_LED_2, BRD_LED_3, BRD_LED_4};
+#endif
+
+uint32_t DoLedSwitch = 0;
+uint32_t DoErrSet = 0;
+
+uint32_t IRQCount = 0;
+uint32_t LEDCount = 0;
+
+int main()
+{
+  uint32_t ledShowSel;
+  uint32_t i;
+  
+  TIMER_CntInitTypeDef TimerInitStruct;
+  
+  //  Clock 80MHz
+  BRD_Clock_Init_HSE_PLL(10);
+  //  LEDs
+  BRD_LEDs_Init();
+  BRD_LED_Set(LED_SEL_ALL, 0);
+  
+  //  Timer
+  BRD_Timer_InitStructDef(&TimerInitStruct);
+  BRD_Timer_Init(&brdTimer1, &TimerInitStruct);
+  
+  while (1)
+  {
+    if (LEDCount != IRQCount)
+    {  
+      LEDCount = IRQCount;
+      
+      //  Led Show
+      ledShowSel = 0;
+      for (i = 0; i < LED_COUNT; ++i)
+        if (LEDCount & (1 << i))
+          ledShowSel |= LED_Sel[i];
+      
+      BRD_LED_Set(LED_SEL_ALL, 0);
+      BRD_LED_Set(ledShowSel, 1);
+    }
+  }
+}
+
+
+#if defined (USE_BOARD_VE_8)
+  #define TimerX_IRQHandler     INT_TMR1_Handler
+
+#elif defined (USE_BOARD_VE_1)
+  #define TimerX_IRQHandler     TIMER1_IRQHandler
+
+#else
+  #define TimerX_IRQHandler     Timer1_IRQHandler
+
+#endif
+
+void TimerX_IRQHandler (void)  
+{
+  IRQCount++;    
+
+#ifdef BUG_ACTIVATE  
+  MDR_TIMER1->STATUS &= ~TIMER_STATUS_CNT_ARR;
+#else  
+  TIMER_ClearITPendingBit (MDR_TIMER1, TIMER_STATUS_CNT_ARR);
+#endif  
+}
+
